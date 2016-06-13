ea49b754e12a91bd644e8a07a5da7d51ac05ced5
PhantasWeng <bensonuong@gmail.com>
2016-06-13 23:08:36 +0800

    Initialize resume project

---
+++ config.rb
@@ -0,0 +1,18 @@
+
+# 匯入 Bootstrap v4
+additional_import_paths = "./bower_components/bootstrap/scss/"
+http_path = "/"
+css_path = "public/stylesheets"
+sass_path = "source/sass"
+images_dir = "source/images"
+javascript_dir = "source/js"
+
+output_style = :nest
+output_style = :expanded
+relative_assets = true
+line_comments = false
+sass_options = {:debug_info=>false}
+sourcemap = true
+asset_cache_buster = false
+preferred_syntax = :sass
+
